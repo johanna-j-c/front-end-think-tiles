b2a6e68eab85aa2e5def98c1402524e84e3b176d
Johanna Cabanban <johanna.cabanban@gmail.com>
2023-08-11 00:22:08 -0400

    Change fraction strips in ImageList and img folder and udpated Board size in App.css

--- src/ImageList.js
+++ src/ImageList.js
@@ -2,6 +2,12 @@ import OneWhole from "./img/OneWhole.png"
 import OneHalf from "./img/OneHalf.png"
 import OneThird from "./img/OneThird.png"
 import OneFourth from "./img/OneFourth.png"
+import OneFifth from "./img/OneFifth.png"
+import OneSixth from "./img/OneSixth.png"
+import OneSeventh from "./img/OneSeventh.png"
+import OneEighth from "./img/OneEighth.png"
+import OneNinth from "./img/OneNinth.png"
+import OneTenth from "./img/OneTenth.png"
 
 const FractionPictures = [
     {
@@ -27,6 +33,42 @@ const FractionPictures = [
         title: OneFourth,
         value: "1/4",
         altText: "One Fourth Fraction Strip"
+    },
+    {
+        id: 5,
+        title: OneFifth,
+        value: "1/5",
+        altText: "One Fifth Fraction Strip"
+    },
+    {
+        id: 6,
+        title: OneSixth,
+        value: "1/6",
+        altText: "One Sixth Fraction Strip"
+    },
+    {
+        id: 7,
+        title: OneSeventh,
+        value: "1/7",
+        altText: "One Seventh Fraction Strip"
+    },
+    {
+        id: 8,
+        title: OneEighth,
+        value: "1/8",
+        altText: "One Eighth Fraction Strip"
+    },
+    {
+        id: 9,
+        title: OneNinth,
+        value: "1/9",
+        altText: "One Ninth Fraction Strip"
+    },
+    {
+        id: 10,
+        title: OneTenth,
+        value: "1/10",
+        altText: "One Tenth Fraction Strip"
     }
 ]
 
